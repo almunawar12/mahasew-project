0d2475438107de227952778555561d9848f1aebf
almunawar12 <rifkialmunawar34@gmail.com>
2026-04-23 22:42:54 +0700

    fixxx

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,6 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: "./prisma/schema.prisma",
-  datasource: {
-    // For migrations and CLI, we use the direct URL
-    url: env("DIRECT_URL") || env("DATABASE_URL"),
-  },
+  schema: './prisma/schema.prisma',
 });
